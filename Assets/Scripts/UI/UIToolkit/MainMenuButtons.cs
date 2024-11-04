b66b4bc2f8b0218c2fd3c51e59bc9f34f323afa1
allan930708 <148655828+Aoli3667@users.noreply.github.com>
2024-11-04 03:15:37 -0800

    ui update
    main menu update

---
+++ Assets/Scripts/UI/UIToolkit/MainMenuButtons.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+
+public class MainMenuButtons : MonoBehaviour
+{
+    public static UnityEvent settingClick = new UnityEvent();
+
+    void Start()
+    {
+        VisualElement root = GetComponent<UIDocument>().rootVisualElement;
+        if (root != null)
+        {
+            root.Q<Button>("Start").clicked += () => StartGame();
+            root.Q<Button>("Setting").clicked += () => Setting();
+            root.Q<Button>("Quit").clicked += () => QuitGame();
+
+            //root.Query<Button>().ForEach(button => button.RegisterCallback<FocusInEvent>(evt => OnSelected(button)));
+            //root.Query<Button>().ForEach(button => button.RegisterCallback<FocusOutEvent>(evt => OnDeSelected(button)));
+        }
+        root.Q<Button>("Start").Focus();
+
+        UIController.finishSetting.AddListener(BackFromSetting);
+    }
+
+    private void OnSelected(Button b)
+    {
+        b.style.backgroundColor = Color.gray;
+    }
+
+    private void OnDeSelected(Button b)
+    {
+        b.style.backgroundColor = new StyleColor(new Color32(188, 188, 188, 255));
+        //b.style.backgroundColor = Color.white;
+    }
+
+    private void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    //可以換成New Game / Load Game
+    private void StartGame()
+    {
+        SceneManager.LoadScene("PrototypeScene");
+    }
+
+    private void Setting()
+    {
+        settingClick.Invoke();
+    }
+
+    private void BackFromSetting()
+    {
+        GetComponent<UIDocument>().rootVisualElement.Q<Button>("Setting").Focus();
+    }
+}
