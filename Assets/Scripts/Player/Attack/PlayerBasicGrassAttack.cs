5233f090094a75fb06d07b2bc1c679bbc51be031
zsj <Jack_zhang.sj@outlook.com>
2024-11-13 08:44:03 -0800

    Basic Grass Attack

---
+++ Assets/Scripts/Player/Attack/PlayerBasicGrassAttack.cs
@@ -0,0 +1,69 @@
+using UnityEngine;
+using UnityEngine.InputSystem.XR;
+using UnityEngine.InputSystem;
+
+public class PlayerBasicGrassAttack : PlayerBasicAttack
+{
+    private bool inAttackFrames;
+
+
+
+    private void OnEnable()
+    {
+        EventHandler.AttackCheckStartEvent += OnAttackCheckStartEvent;
+        EventHandler.AttackCheckEndEvent += OnAttackCheckEndEvent;
+        Init();
+    }
+
+    private void OnDisable()
+    {
+        EventHandler.AttackCheckStartEvent -= OnAttackCheckStartEvent;
+        EventHandler.AttackCheckEndEvent -= OnAttackCheckEndEvent;
+    }
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        InputAction attackAction = controller.GetAttackAction();
+
+    }
+
+
+    private void OnAttackCheckStartEvent()
+    {
+        Debug.Log("start check attack collision" + Time.realtimeSinceStartup);
+        inAttackFrames = true;
+    }
+    private void OnAttackCheckEndEvent()
+    {
+        Debug.Log("end check attack collision" + Time.realtimeSinceStartup);
+        inAttackFrames = false;
+    }
+
+    public override void Attack()
+    {
+        base.Attack();
+
+        anim.Play();
+    }
+
+
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        Debug.Log("check attack collision: hit an enemy");
+        if (collision.tag == "Enemy" && inAttackFrames)
+        {
+            Debug.Log("check attack collision: hit an enemy" + Time.realtimeSinceStartup);
+            Destroy(collision.gameObject);
+        }
+    }
+}
