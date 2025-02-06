b7a84298f2d47caf057842a00e826e18a9668066
Aoli3667 <148655828+Aoli3667@users.noreply.github.com>
2025-02-05 19:42:05 -0800

    Revert "Merge branch 'Allanhihi' into AllanLastOneIPromise"
    This reverts commit b034f6ce33e6d0c91f025c9ed35411c4f50768cf, reversing
    changes made to 2cad6c1228794cfd5b0bcdba46a9ba6e760966de.

--- Assets/Scripts/Player/Attack/PlayerBasicFireAttack.cs
+++ Assets/Scripts/Player/Attack/PlayerBasicFireAttack.cs
@@ -2,16 +2,12 @@ using UnityEngine;
 using UnityEngine.InputSystem.XR;
 using UnityEngine.InputSystem;
 using System.Collections;
-using UnityEditorInternal;
 
 public class PlayerBasicFireAttack : PlayerBasicAttack
 {
     [SerializeField] private bool inAttackFrames;
     [SerializeField] private SpriteRenderer sprd;
-    [SerializeField] private Animator fireAttackAnim;
-
-    [SerializeField] private float timeLength;
-
+    [SerializeField] private Animation fireAttackAnim;
 
 
     private void OnEnable()
@@ -33,7 +29,7 @@ public class PlayerBasicFireAttack : PlayerBasicAttack
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
 
     }
 
@@ -59,36 +55,24 @@ public class PlayerBasicFireAttack : PlayerBasicAttack
     {
         base.Attack();
 
-        //Debug.Log("fire attack");
-        //if(!sprd.enabled)
-        //    sprd.enabled = true;
-        //inAttackFrames = true;
+        Debug.Log("fire attack");
+        if(!sprd.enabled)
+            sprd.enabled = true;
+        inAttackFrames = true;
 
-        ////anim.Play();
-        //StopAllCoroutines();
-        //StartCoroutine(StopPlayAnim());
-
-        controller.playerMove.FreeControl(timeLength);
-        fireAttackAnim.SetBool("canAttack", true);
-        //fireAttackAnim.Play("fireAttack");
+        //anim.Play();
+        StopAllCoroutines();
         StartCoroutine(StopPlayAnim());
-        sprd.enabled = true;
-
     }
 
     IEnumerator StopPlayAnim()
     {
-        //yield return new WaitForSeconds(0.5f);
-        //inAttackFrames = false;
-        //if (sprd.enabled)
-        //    sprd.enabled = false;
-        ////fireAttackAnim.Stop();
-        //Debug.LogWarning("Fire in atacck end ");
-        yield return new WaitForSeconds(timeLength);
-        fireAttackAnim.SetBool("canAttack", false);
-        sprd.enabled = false;
-
-
+        yield return new WaitForSeconds(0.5f);
+        inAttackFrames = false;
+        if (sprd.enabled)
+            sprd.enabled = false;
+        //fireAttackAnim.Stop();
+        Debug.LogWarning("Fire in atacck end ");
     }
 
 
