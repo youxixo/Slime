1a1d1261bc64575df3bcc52761a955d4bf8c0c04
Aoli3667 <148655828+Aoli3667@users.noreply.github.com>
2025-03-18 09:36:29 -0700

    Boss fight camera change and music change

---
+++ Assets/Scripts/Prop/CameraChanger.cs
@@ -0,0 +1,42 @@
+using NUnit.Framework;
+using Unity.Cinemachine;
+using UnityEngine;
+
+public class CameraChanger : MonoBehaviour
+{
+    private Collider2D ogCollider;
+    [SerializeField]private Transform lockPoint;
+    [SerializeField] private CinemachineConfiner2D cine_camera;
+    [SerializeField] private CinemachineCamera CinemachineCamera;
+    [SerializeField] AudioClip bossMusic;
+    [SerializeField] AudioSource source;
+    
+    private void ActiveChild()
+    {
+        foreach(Transform child in transform)
+        {
+            child.gameObject.SetActive(true);
+        }
+    }
+
+    private void DeactiveChild()
+    {
+        foreach (Transform child in transform)
+        {
+            child.gameObject.SetActive(false);
+        }
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            Camera.main.transform.position = new Vector3(lockPoint.position.x, lockPoint.position.y, Camera.main.transform.position.z);
+            this.CinemachineCamera.Follow = lockPoint;
+            source.clip = bossMusic;
+            source.Play();
+            ActiveChild();
+        }
+    }
+}
